d8be44b941cc8362263f349de988e514a77b5de6
U-espen-store\Espen <Espen@espen-store.(none)>
2011-10-10 17:45:56 +0200

    parametre i rest kall

--- src/main/java/no/naf/aktivitetsadmin/rest/Categories.java
+++ src/main/java/no/naf/aktivitetsadmin/rest/Categories.java
@@ -6,7 +6,10 @@ import com.sun.jersey.api.client.WebResource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
 import static no.naf.aktivitetsadmin.ClientContainer.client;
 
@@ -15,9 +18,11 @@ public class Categories {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getCategories() {
+    public String getCategories(@Context UriInfo ui) {
         System.out.println("categories GET");
+        MultivaluedMap<String, String> qp = ui.getQueryParameters();
         WebResource r = client.resource("http://naf.herokuapp.com/categories");
+        r = r.queryParams(qp);
         return r.get(String.class);
 
     }
--- src/main/java/no/naf/aktivitetsadmin/rest/Locations.java
+++ src/main/java/no/naf/aktivitetsadmin/rest/Locations.java
@@ -6,7 +6,10 @@ import com.sun.jersey.api.client.WebResource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
 import static no.naf.aktivitetsadmin.ClientContainer.client;
 
@@ -15,10 +18,11 @@ public class Locations {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getCategories() {
-        System.out.println("location GET");
-
+    public String getLocations(@Context UriInfo ui) {
+        System.out.println("locations GET");
+        MultivaluedMap<String, String> qp = ui.getQueryParameters();
         WebResource r = client.resource("http://naf.herokuapp.com/locations.json");
+        r = r.queryParams(qp);
         return r.get(String.class);
 
     }
