e0291ce5923d0c280a54a451e93850119e239ccd
U-espen-store\Espen <Espen@espen-store.(none)>
2011-10-10 15:12:54 +0200

    flytta ut kategori navn henting i egen metode lagt til search tool i header

--- src/main/webapp/app/view/activity/List.js
+++ src/main/webapp/app/view/activity/List.js
@@ -1,17 +1,22 @@
-Ext.define('NAF.view.activity.List' ,{
+Ext.define('NAF.view.activity.List', {
     extend: 'Ext.grid.Panel',
     alias : 'widget.activitylist',
     store: 'Activities',
     collapsible: true,
+    tools: [
+        {type: 'search', handler: this.search}
+    ],
 
     title : 'Alle aktiviteter',
 
-    dockedItems: [{
-        xtype: 'pagingtoolbar',
-        store: 'Activities',   // same store GridPanel is using
-        dock: 'bottom',
-        displayInfo: true
-    }],
+    dockedItems: [
+        {
+            xtype: 'pagingtoolbar',
+            store: 'Activities',   // same store GridPanel is using
+            dock: 'bottom',
+            displayInfo: true
+        }
+    ],
 
     initComponent: function() {
 
@@ -19,18 +24,23 @@ Ext.define('NAF.view.activity.List' ,{
         this.columns = [
             {header: 'Dato',  dataIndex: 'dtstart',  flex:1, format: 'c'},
             {header: 'Navn',  dataIndex: 'summary',  flex: 2},
-            {header: 'Kategori', dataIndex: 'category_id', flex: 2, renderer: function(value){
-                var store = Ext.getStore('Categories');
-                var category = store.getById(value);
-                return category.get('name');
-            }}
+            {header: 'Kategori', dataIndex: 'category_id', flex: 2, renderer: this.findCategoryName
+            }
         ];
 
 
-
-
         this.callParent(arguments);
-    }
+    },
 
+    findCategoryName: function(value) {
+        var store = Ext.getStore('Categories');
+        var category = store.getById(value);
+        return category.get('name');
+    },
+
+
+    search: function() {
+        console.log('search');
+    }
 
 });
